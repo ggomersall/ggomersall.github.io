e6b5efbedc1d890c5bb83c2e165034b5dfb8f332
Gareth Gomersall <ggomersall@gmail.com>
2016-01-06 12:23:02 +0000

    changed modal type for project views

--- js/creative.js
+++ js/creative.js
@@ -45,4 +45,16 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+    // modal styling
+    $('div.modal').on('show.bs.modal', function() {
+        var modal = this;
+        var hash = modal.id;
+        window.location.hash = hash;
+        window.onhashchange = function() {
+            if (!location.hash){
+                $(modal).modal('hide');
+            }
+        }
+    });
+
 })(jQuery); // End of use strict
